3d0d11978c73bebdb3358015b627ac7da2581d09
Jose Alberto <alberto22mx@gmail.com>
2026-09-10 18:28:56 -0600

    chore: initialize Spring Boot project

---
+++ src/main/java/com/alberto/tvmaze/TvmazeMiddlewareApiApplication.java
@@ -0,0 +1,13 @@
+package com.alberto.tvmaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeMiddlewareApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvmazeMiddlewareApiApplication.class, args);
+    }
+
+}
